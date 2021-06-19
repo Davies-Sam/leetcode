f47ea04bb040e3ce2b94aee450d5b95cfa8b2ae4
Sam Davies <davies.sam1994@gmail.com>
2021-06-19 14:49:07 -0400

    add solution

--- removeDupSortedArray/src/App.java
+++ removeDupSortedArray/src/App.java
@@ -2,4 +2,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int removeDuplicates(int[] nums) {
+        
+        HashSet<Integer> seen = new HashSet<Integer>();
+        int insertionIndex = 0;
+        for(int num : nums) {
+            if(!seen.contains(num)){
+                seen.add(num);
+                nums[insertionIndex] = num;
+                insertionIndex ++;
+            }
+        }
+        
+        return insertionIndex;
+    }
 }
--- removeElement/src/App.java
+++ removeElement/src/App.java
@@ -2,4 +2,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static int removeElement(int[] nums, int val) {
+        
+        int insertionIndex = 0;
+        for(int num : nums) {
+            if(num != val) {
+                nums[insertionIndex] = num;
+                insertionIndex++;
+            }
+        }
+        return insertionIndex;
+    }
 }
