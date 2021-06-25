ae3e90ee0a21d0a5f3492bfd04ada4955da751a6
Sam Davies <davies.sam1994@gmail.com>
2021-06-25 11:24:06 -0400

    add output

--- removeDupSortedArray/src/App.java
+++ removeDupSortedArray/src/App.java
@@ -1,6 +1,11 @@
+import java.util.HashSet;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] nums = new int[] {1,2,2,3,3,3,4,4,4,4,5,5,5,5,5,6,6,6,6,6,6};
+        int newLength = removeDuplicates(nums);
+        for(int i = 0; i < newLength; i++) {
+            System.out.print(nums[i]);
+        }
     }
 
     public static int removeDuplicates(int[] nums) {
