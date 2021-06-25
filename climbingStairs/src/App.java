480127d88abb6f3615643c112f76f09ae6832f56
Sam Davies <davies.sam1994@gmail.com>
2021-06-25 17:05:57 -0400

    climbing stairs

---
+++ climbingStairs/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+        System.out.println(climbingStairs(12));
+    }
+
+    public static int climbingStairs(int n) {
+        /* use the basics to solve this question
+        and find that its the fibonacci problem.
+        each stair number can be calculated using
+        the number of distinct ways to reach 1 step
+        below + # of ways for 2 steps below.
+        if you are one or two steps away from your 
+        target step, we already know from our base
+        case the number of ways to go both 1 and 2 steps.
+        */
+
+        int[] memo = new int[]{1,2};
+        int next = 0;
+        int count = 1;
+        while(count < n){
+            next = memo[0]+memo[1];
+            memo[0] = memo[1];
+            memo[1] = next;
+            count++;   
+        }
+        return memo[0];  
+    }
+}
